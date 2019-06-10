a882632a7ead59dd849fb7f82ba6fcfc73f1cf2c
LAPTOP-VACUB08O\Антон <megadeathlightsaber@gmail.com>
2019-06-10 20:35:48 +0700

    Graphics routines moved to separate package

--- graphics/graphics.go
+++ graphics/graphics.go
@@ -1,4 +1,4 @@
-package main
+package graphics
 
 import (
 	"image/color"
@@ -124,60 +124,4 @@ type vertex2D struct {
 	x      float64
 	y      float64
 	inPath bool
-}
-
-// renderGraph рисует по заданным данным граф и сохраняет изображение в png-файл output
-func renderGraph(output string, vertexCount int, isDirected bool, isWeighted bool, isColored bool, names []string, path []int, matrix [][]int, weights [][]string, colors []uint32, colorCover [][]int) {
-	positions := make([]vertex2D, vertexCount)
-	combination := randomCombination(vertexCount*vertexCount, vertexCount)
-
-	// imgSide := vertexCount * CellSide
-	imgSide := (vertexCount + 1) * CellSide
-
-	for i := 0; i < vertexCount; i++ {
-		positions[i].x = float64(combination[i]%vertexCount+1) * CellSide
-		positions[i].y = float64(combination[i]/vertexCount+1) * CellSide
-		positions[i].inPath = false
-	}
-
-	for _, val := range path {
-		positions[val].inPath = true
-	}
-
-	context := generateContext(imgSide, imgSide)
-
-	for i := 0; i < vertexCount; i++ {
-		drawVertex(context, names[i], positions[i].x, positions[i].y, VertexRadius, positions[i].inPath, isColored, colors, colorCover[i][i])
-		for j := 0; j < vertexCount; j++ {
-			if matrix[i][j] == 1 {
-				drawEdge(context, positions[i].x, positions[i].y, positions[j].x, positions[j].y, VertexRadius, isDirected, isColored, colors, colorCover[i][j])
-				if isWeighted {
-					drawEdgeWeight(context, weights[i][j], positions[i].x, positions[i].y, positions[j].x, positions[j].y)
-				}
-			}
-		}
-	}
-
-	if len(path) > 0 {
-		firstPathID := path[0]
-		lastPathID := path[len(path)-1]
-
-		context.SetRGB255(0, 0, 255)
-		context.DrawCircle(positions[firstPathID].x, positions[firstPathID].y, VertexRadius)
-		context.Stroke()
-
-		context.SetRGB255(255, 0, 0)
-		context.DrawCircle(positions[lastPathID].x, positions[lastPathID].y, VertexRadius)
-		context.Stroke()
-	}
-
-	context.SetColor(color.Black)
-	context.DrawString("Original Graph", 10, 15)
-	outW, outH := context.MeasureString(output)
-	context.Push()
-	context.SetRGBA255(0, 0, 0, 125)
-	context.DrawString(output, float64(imgSide)-outW-8, float64(imgSide)-outH+8)
-	context.Pop()
-
-	context.SavePNG(output)
 }
--- middleware.go
+++ middleware.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"graph-labs/tinyviz/graphics"
 	"graph-labs/tinyviz/legacy"
 	"graph-labs/tinyviz/random"
 	"log"
@@ -17,7 +18,7 @@ func visualize(fileName string) error {
 
 	random.ShuffleSeed()
 
-	renderGraph(fmt.Sprintf("%s.viz.png", fileName), count, isDirected, isWeighted, isColored, names, path, matrix, weights, colors, colorMatrix)
+	graphics.RenderGraph(fmt.Sprintf("%s.viz.png", fileName), count, isDirected, isWeighted, isColored, names, path, matrix, weights, colors, colorMatrix)
 	fmt.Printf("%s visualizated\n", fileName)
 
 	return nil
