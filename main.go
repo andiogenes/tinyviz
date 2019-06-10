d7e3386344df2dec247537175ef2abb5a1b957f3
LAPTOP-VACUB08O\Антон <megadeathlightsaber@gmail.com>
2019-06-10 19:48:46 +0700

    CLI now is managed by urfave/cli framework

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"path/filepath"
 	"sync"
+
+	"github.com/urfave/cli"
 )
 
 // visualize визуализирует граф на основе информации из дескриптора
@@ -76,14 +78,56 @@ func visualizeFolder() {
 }
 
 func main() {
-	// Если конкретный дескриптор задан в аргументах командной строки,
-	// то запускается визуализация только по нему
-	// Иначе: визуализируются все графы по имеющимся в папке дескрипторам
-	if len(os.Args) >= 2 {
-		if err := visualize(os.Args[1]); err != nil {
-			log.Fatal(err)
+	var format string
+	var quality int
+
+	app := cli.NewApp()
+
+	app.Name = "tinyviz"
+	app.Version = "0.2.0"
+	app.Description = "Graph visualization tool for educational purposes"
+	app.Authors = []cli.Author{
+		{Name: "Anton", Email: "megadeathlightsaber@gmail.com"},
+	}
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "format, f",
+			Value:       "png",
+			Usage:       "image output format (jpg/png)",
+			Destination: &format,
+		},
+		cli.IntFlag{
+			Name:        "quality, q",
+			Value:       80,
+			Usage:       "jpeg image ouput quality (from 0 to 100)",
+			Destination: &quality,
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		if format != "png" && format != "jpg" && format != "jpeg" {
+			fmt.Println("Unknown format \"", format, "\", reset to \"png\"")
+			format = "png"
 		}
-	} else {
+
+		if quality <= 0 || quality > 100 {
+			fmt.Println("Unbounded quality value ", quality, ", reset to 80")
+			quality = 80
+		}
+
+		if c.NArg() > 0 {
+			err := visualize(c.Args()[0])
+			return err
+		}
+
 		visualizeFolder()
+
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
