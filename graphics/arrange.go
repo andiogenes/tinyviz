f186a505bd1ea00f988d1a54a2e4bb308e28fa80
LAPTOP-VACUB08O\Антон <megadeathlightsaber@gmail.com>
2019-06-13 23:14:01 +0700

    Arrangement rules for RenderGraph lie her

---
+++ graphics/arrange.go
@@ -0,0 +1,16 @@
+package graphics
+
+// arrange.go contains set of built-in arrangement rules for RenderGraph function of render.go
+
+import "graph-labs/tinyviz/random"
+
+// PutVertexInRandomFreeCell ...
+func PutVertexInRandomFreeCell(positions []vertex2D, options RenderOptions) {
+	combination := random.Combination(options.VertexCount*options.VertexCount, options.VertexCount)
+
+	for i := 0; i < options.VertexCount; i++ {
+		positions[i].x = float64(combination[i]%options.VertexCount+1) * CellSide
+		positions[i].y = float64(combination[i]/options.VertexCount+1) * CellSide
+		positions[i].inPath = false
+	}
+}
