e36f07d669277dc60849ee983612832e2c8bc3dd
LAPTOP-VACUB08O\Антон <megadeathlightsaber@gmail.com>
2019-06-18 12:59:50 +0700

    Data loader functions for vertex arrangement

---
+++ input/arrangeLoader.go
@@ -0,0 +1,33 @@
+package input
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+// ArrangementLoader ...
+type ArrangementLoader func(string) (interface{}, error)
+
+type coordRepresentation struct {
+	Coords [][]int `json:"coordinates"`
+}
+
+type concreteCoords struct{ coords [][]int }
+
+// CoordinatesLoader ...
+func CoordinatesLoader(fileName string) (struct{ coords [][]int }, error) {
+	f, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return concreteCoords{}, err
+	}
+
+	// Unmarshall data
+	var cRep coordRepresentation
+	if err = json.Unmarshal(f, &cRep); err != nil {
+		return concreteCoords{}, err
+	}
+
+	retVal := concreteCoords{coords: cRep.Coords}
+
+	return retVal, nil
+}
